e09485ccf67c9193b5dd86ae5d3a6208391043ce
Noufel17 <jn_naili@esi.dz>
2023-01-16 21:40:31 +0100

    added la machine abstraite des quadreplets

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "quadreplet.h"
+
+void main(){
+    quad q = creerQuadreplet("+","a","b","R1",1);
+    q = insererQuadreplet(q,"-","a","c","R2",2);
+    q = insererQuadreplet(q,"*","R1","R2","R3",3);
+    q = insererQuadreplet(q,"BR","etiq","","R3",4);
+    q = insererQuadreplet(q,"-","d","e","R4",5);
+    q = insererQuadreplet(q,"/","R2","R3","R5",6);
+    afficherQuad(q);
+    updateQuadreplet(q,4,"6");
+    enregistrerQuad(q);
+}
---
+++ quadreplet.h
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// la structure QUAD (qui contienne les quadreplets) sera implémentée comme 
+// liste lineare chainée de chaines de caractères
+typedef struct 
+{   
+    char operateur[30];
+	char operande1[30];
+	char operande2[30];   
+	char resultat[30];   
+	int qc;    //it's named qc par convontion
+    struct quadreplet *suivant; // suivant pour liste lineare chainée
+}quadreplet;
+typedef quadreplet* quad;
+
+// machine abstraite 
+
+quad creerQuadreplet(char opr[30],char op1[30],char op2[30],char res[30],int num){
+    quad q = malloc(sizeof(quadreplet));
+    strcpy(q->operateur,opr);
+    strcpy(q->operande1,op1);
+    strcpy(q->operande2,op2);
+    strcpy(q->resultat,res);
+    q->qc=num;
+    q->suivant=NULL;
+    return q;
+}
+
+quad insererQuadreplet(quad p,char opr[],char op1[],char op2[],char res[],int num) {  
+    quad q;
+    if(p==NULL){
+        p=creerQuadreplet(opr,op1,op2,res,num);
+        return p;
+    }else{
+    q = malloc(sizeof(quadreplet));
+    strcpy(q->operateur,opr);
+    strcpy(q->operande1,op1);
+    strcpy(q->operande2,op2);
+    strcpy(q->resultat,res);
+    q->qc=num;
+    q->suivant = p;
+    p = q;
+        return p;
+    }
+}
+
+// mise a jour du quadreplet numero qc dans le quad (l'ensemble des quadreplets)
+quad updateQuadreplet(quad q, int qc,char num[30]){
+    quad p = q;
+    if (p==NULL){
+        return q;
+    }
+    while(p!=NULL){
+        if(p->qc==qc){
+            strcpy(p->operande1,num);
+            return p;
+        }
+        p=p->suivant;
+    }
+    return q;
+}
+void afficherQuad(quad q)
+{
+    printf("\n<<<<<<<<<<  Affichage des quads  >>>>>>>>>>\n");
+    if (q==NULL){
+     printf("\n\n \t\t Quad Vide \n");
+    }else{
+    printf("**********************************************\n");
+        while(q!=NULL){   
+            printf("\t Quad[%d]=[ %s , %s , %s , %s ] \n",q->qc,q->operateur,q->operande1,q->operande2,q->resultat);
+            q=q->suivant; 
+        }
+    }
+    printf("**********************************************\n");
+}
+
+// ecrire le Quad dans un fichier
+void enregistrerQuad(quad q){
+    FILE* fp;
+    fp=fopen("quadreplets.txt","w");
+    if(fp==NULL){
+        printf("\n\n \t\t Erreur lors de l'ouverture du fichier\n");
+    }
+    if (q==NULL){
+     printf("\n\n \t\t Quad Vide \n");
+    }else{
+        while(q!=NULL){   
+            fprintf(fp,"Quad[%d]=[ %s , %s , %s , %s ] \n",q->qc,q->operateur,q->operande1,q->operande2,q->resultat);
+            q=q->suivant; 
+        }
+    }
+    fclose(fp);
+}
